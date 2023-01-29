e1a13c773ac8f74d7b546039d6b416edaf1bc9d0
kkito0726 <k.kito0726@gmail.com>
2023-01-29 15:47:39 +0900

    画像投稿機能を実装

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,6 +1,6 @@
 import { Analytics, Face, Gif, Image } from "@mui/icons-material";
 import axios from "axios";
-import React, { useContext, useRef } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { AuthContext } from "../../state/AuthContext";
 import "./Share.css";
 
@@ -8,6 +8,8 @@ export default function Share() {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
   const { user } = useContext(AuthContext);
   const desc = useRef();
+
+  const [file, setFile] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,6 +19,19 @@ export default function Share() {
       desc: desc.current.value,
     };
 
+    if (file) {
+      const data = new FormData();
+      const fileName = Date.now() + file.name;
+      data.append("name", fileName);
+      data.append("file", file);
+      newPost.img = fileName;
+      try {
+        // 画像APIを叩く
+        await axios.post("/upload", data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
     try {
       await axios.post("/posts", newPost);
       window.location.reload();
@@ -53,10 +68,17 @@ export default function Share() {
           }}
         >
           <div className="shareOptions">
-            <div className="shareOption">
+            <label className="shareOption" htmlFor="file">
               <Image className="shareIcon" htmlColor="blue" />
               <span className="shareOptionText">写真</span>
-            </div>
+              <input
+                type="file"
+                id="file"
+                accept=".png, .jpeg, .jpg"
+                style={{ display: "none" }}
+                onChange={(e) => setFile(e.target.files[0])}
+              />
+            </label>
 
             <div className="shareOption">
               <Gif className="shareIcon" htmlColor="hotpink" />
