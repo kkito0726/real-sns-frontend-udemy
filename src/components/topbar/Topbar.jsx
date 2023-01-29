b25d5208f3cc596612942e7f845b9783d5c2f95e
kkito0726 <k.kito0726@gmail.com>
2023-01-29 15:50:00 +0900

    logを削除

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -9,7 +9,6 @@ export default function Topbar() {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
 
   const { user } = useContext(AuthContext);
-  console.log(user);
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
